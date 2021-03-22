47bd2eeb9d69fae8c1e06180e7173d58e969de42
prateekeshwar <prateekeshwar@paymeindia.in>
2021-03-22 09:27:29 +0530

    refactored so that Timer.js solely consist of timer functionality

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,61 +1,76 @@
 import React, { useEffect, useState, useRef } from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import './style.css';
 
-export default function Timer({timer, startGame, wordMatch}) {
+export default function Timer({
+  timer,
+  wordChange,
+  handleWordChange,
+  stopGame,
+}) {
+  const [time, settime] = useState(timer);
+  const [currInterval, setcurrInterval] = useState('');
+  const [circleDashArray, setcircleDashArray] = useState('283, 283');
+  const countRef = useRef(time);
+  countRef.current = time;
+  const countarr = useRef(circleDashArray);
+  countarr.current = circleDashArray;
 
-    const [time,  settime] = useState(timer)
-    const [currTimer, setcurrTimer] = useState(timer)
-    const [gameCount] = useState(JSON.parse(window.sessionStorage.getItem("gameCount")))
-    const [scoreList] = useState(JSON.parse(window.sessionStorage.getItem("scoreList")))
-    const countRef = useRef(time);
-    const wordMatchedRef = useRef(wordMatch);
-    wordMatchedRef.current = wordMatch;
-    countRef.current = time;
-
-    useEffect(()=> {
-        if (startGame) {
-        if (currTimer !== timer) {
-            settime(timer)
-            setcurrTimer(timer)
-        const interval = setInterval(() => {
-                      if (wordMatchedRef.current) {
-                clearInterval(interval);
-              window.sessionStorage.setItem("gameCount", gameCount + 1)
-              window.sessionStorage.setItem("scoreList", JSON.stringify([...scoreList,
-                ...[{id:gameCount+1, score:((timer-countRef.current)/100).toFixed(2).replace(/\./g, ':') }]]))
-              window.location.reload();
-                  return;
-            }
-            const currCount = countRef.current;
-            if (currCount <= 0) {
-            clearInterval(interval);
-            window.location.pathname = '/game-over'
-            return;
-            }
-            settime(currCount -1)
-          }, 10);
+  useEffect(() => {
+    if (wordChange) {
+      clearTimeout(currInterval);
+      setcircleDashArray('283, 283');
+      settime(timer);
+      handleWordChange();
+      const interval = setInterval(() => {
+        const currCount = countRef.current;
+        if (currCount <= 0) {
+          setcircleDashArray('283, 283');
+          clearInterval(interval);
+          stopGame();
+          return;
         }
+        const fraction = (currCount-1)/timer;
+        const circleDashArr = `${(
+            fraction * 283
+        ).toFixed(0)}, 283`;
+        setcircleDashArray(circleDashArr);
+        settime(currCount - 1);
+      }, 10);
+      setcurrInterval(interval);
     }
-    })
-
-    const getTimerFormat = () => {
-        if (!startGame) {
-            return timer === 0 ? "0:00" : (timer/100).toFixed(2).replace(/\./g, ':') 
-        }
-        return (time/100).toFixed(2).replace(/\./g, ':')
-    }
+  });
 
   return (
     <div className="timer-circle">
-        <p className="timer">{getTimerFormat()}</p>
+      <svg
+        className="base-timer__svg"
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g className="base-timer__circle">
+          <circle className="base-timer__path-elapsed" cx="50" cy="50" r="45" />
+          <path
+            id="base-timer-path-remaining"
+            strokeDasharray={circleDashArray}
+            className="base-timer__path-remaining"
+            d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+          />
+        </g>
+      </svg>
+      <p className="timer">{(time / 100).toFixed(2).replace(/\./g, ':')}</p>
     </div>
   );
 }
 
 Timer.propTypes = {
-    timer: PropTypes.number.isRequired,
-    startGame: PropTypes.bool.isRequired,
-    wordMatch: PropTypes.bool.isRequired
-  }
-  
+  timer: PropTypes.number.isRequired,
+  wordChange: PropTypes.bool.isRequired,
+  handleWordChange: PropTypes.func.isRequired,
+  stopGame: PropTypes.func.isRequired,
+};
