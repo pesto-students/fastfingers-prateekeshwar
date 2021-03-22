0dd6537331995f8ab87c22a32bd254068f60b20c
prateekeshwar <prateekeshwar@paymeindia.in>
2021-03-22 08:45:11 +0530

    added game console dashboard file

--- src/components/GameConsole.js
+++ src/components/GameConsole.js
@@ -1,20 +1,89 @@
-import React, {} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './style.css';
-import PlayerConfig from "./PlayerConfig"
-import ScoreBoard from "./ScoreBoard"
-// import Timer from "./Timer"
-import WordInput from "./WordInput"
+import PlayerConfig from './PlayerConfig';
+import ScoreBoard from './ScoreBoard';
+import WordInput from './WordInput';
+import stopGameIcon from '../assets/stop-game-icon/stop-game-icon.png';
 
 export default function GameConsole() {
-    return (
-    <div className="general-flex-direction-row">
-        <div>
-<PlayerConfig />
-<ScoreBoard />
-</div>
-<div>
-<WordInput/>
-</div>
+  const [gameCount] = useState(
+    JSON.parse(window.sessionStorage.getItem('gameCount'))
+  );
+  const [scoreList] = useState(
+    JSON.parse(window.sessionStorage.getItem('scoreList'))
+  );
+  const [second, setsecond] = useState(0);
+  const [minute, setminute] = useState(0);
+  const secondRef = useRef(second);
+  const minuteRef = useRef(minute);
+  secondRef.current = second;
+  minuteRef.current = minute;
+
+  const stopGame = () => {
+    window.sessionStorage.setItem('gameCount', gameCount + 1);
+    window.sessionStorage.setItem(
+      'scoreList',
+      JSON.stringify([
+        ...scoreList,
+        ...[
+          {
+            id: gameCount + 1,
+            score: `${minuteRef.current < 10 ? `0${minuteRef.current}` : minuteRef.current}:${
+                secondRef.current < 10 ? `0${secondRef.current}` : secondRef.current
+            }`,
+          },
+        ],
+      ])
+    );
+    window.location.pathname = '/game-over';
+  };
+
+  useEffect(() => {
+    const updateLevel = setInterval(() => {
+      if (secondRef.current >= 60) {
+        setsecond(0);
+        setminute(minuteRef.current + 1);
+      } else {
+        setsecond(secondRef.current + 1);
+      }
+    }, 1000);
+    return () => {
+      clearTimeout(updateLevel);
+    };
+  }, []);
+
+  return (
+    <div
+      className="general-flex-direction-row"
+      style={{ justifyContent: 'space-between' }}
+    >
+      <div>
+        <PlayerConfig />
+        <ScoreBoard />
+        <div
+          className="start-game-button"
+          style={{ marginTop: '40px' }}
+          onClick={stopGame}
+          aria-hidden="true"
+        >
+          <img src={stopGameIcon} alt="play-img" className="stop-game-icon" />
+          <input
+            type="button"
+            name="stopGame"
+            id="stopGame"
+            value="STOP GAME"
+          />
+        </div>
+      </div>
+      <div>
+        <WordInput stopGame={stopGame}/>
+      </div>
+      <div className="general-flex-direction-column">
+        <p className="sub-heading-fast-finger">fast fingers</p>
+        <p className="score-count">{`SCORE: ${
+          minute < 10 ? `0${minute}` : minute
+        }:${second < 10 ? `0${second}` : second}`}</p>
+      </div>
     </div>
-    )
+  );
 }
